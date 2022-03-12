6d06ef2bb9321681a4a894ed3a68bd5efb886569
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2022-03-12 12:44:45 -0500

    clean code 🧹

--- tree/src.cpp
+++ tree/src.cpp
@@ -5,21 +5,17 @@ string s;
 int p, ans;
 int recursion(int p, int num, int count)
 {
-	if (p==1) return 1;
-	int front=(num-1)+p, back=num, dest;
-	if (count>s.length()-1)
-		dest=recursion(p/2, front+1, count-1);
-	else
-		dest=recursion(p/2, front+1, count-1)*2-(s[count]=='R');
-	if (s[count+1]=='\0'&&ans==0)
-		ans=(back+dest)-1;
-	else
-		return dest;
-	return -1;
+        if (p==1) return 1;
+        int dest=dest=recursion(p/2, num+p, count-1)*2-(s[count]=='R');
+        if (s[count+1]=='\0'&&ans==0)
+                ans=(num+dest)-1;
+        else
+                return dest;
+        return -1;
 }
 int main()
 {
-	cin >> p >> s;
-	recursion(pow(2, p), 1, p-1);
-	cout << ans;
+        cin >> p >> s;
+        recursion(pow(2, p), 1, p-1);
+        cout << ans;
 }
